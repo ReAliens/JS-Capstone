dce8b610736a094b84b7e02625d629444029513a
Ahmed <ahmedreda152@gmail.com>
2021-12-21 12:19:11 +0200

    use the api to bring all poke data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import _ from 'lodash';
+import { getPokeData } from './utils/pokeData.js';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+getPokeData.then((data) => console.log(data));
+// console.log(data);
