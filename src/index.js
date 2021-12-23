a16c150877b1b5eace6c5ca777c088a83865d6f2
Tumworobere <annaht.annah@gmail.com>
2021-12-23 11:07:48 +0300

    display comments Popup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import displayData from './utils/displayData.js';
 import icon from './images/pokemon.png';
-import { countermovie } from './utils/counter.js';
+import enableComments from './utils/CommentPopup';
 
 const logo = document.querySelector('.Pokemon-logo');
 const myIcon = new Image();
