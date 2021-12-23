569d18788c7cf60b0df48d3e09ccf8a7d19d3851
Zemola <azeemolatunji@gmail.com>
2021-12-23 08:32:26 +0100

    Added popup modal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 import './styles.css';
 import displayData from './utils/displayData.js';
 import icon from './images/pokemon.png';
+import popCard from './utils/pop-up.js'
+import reserveButton from "./utils/displayData.js";
+import getData from './utils/pokeData.js';
 
 const logo = document.querySelector('.Pokemon-logo');
 const myIcon = new Image();
 myIcon.src = icon;
 logo.appendChild(myIcon);
 window.onload = displayData;
+
+const displayModal = async () => {
+  const data = await getData();
+  popCard(data);
+  reserveButton.forEach((btn) => {
+    btn.addEventListener("click", async (e) => {
+      if (
+        btn.id === `x-${e.target.classList[1]}` ||
+        btn.id === `x-${e.target.classList[2]}`
+      ) {
+        const pop = document.querySelector("#popup");
+        pop.classList.toggle("active");
+        const cards = document.querySelectorAll(`.c-${btn.id}`);
+        cards.forEach((item) => {
+          item.classList.toggle("hidden");
+        });
+      }
+    });
+  });
+  const clsBtns1 = document.querySelectorAll(".x");
+  clsBtns1.forEach((clsBtn) => {
+    clsBtn.addEventListener("click", async () => {
+      const cards = document.querySelectorAll(`.${clsBtn.classList[1]}`);
+      const pop = document.querySelector("#popup");
+      pop.classList.toggle("active");
+      cards.forEach((card) => {
+        card.classList.toggle("hidden");
+      });
+    });
+  });
+};
+
+displayModal();
---
+++ src/utils/pop-up.js
@@ -0,0 +1,87 @@
+
+
+const popCard = async (data) => {
+  const { movies } = data;
+
+  movies.forEach(async (movie, index) => {
+    if (index < 50) {
+      const pop = document.querySelector("#popup");
+      // creat overlay div
+      const over = document.createElement("div");
+      over.classList.add("overaly");
+      over.classList.add("hidden");
+      over.classList.add(`c-x-${movie.idmovie}`);
+      pop.appendChild(over);
+      // creat content div
+      const content = document.createElement("div");
+      content.classList.add("content");
+      content.classList.add("hidden");
+      content.classList.add(`c-x-${movie.idmovie}`);
+      content.id = `content2-${movie.idmovie}`;
+      pop.appendChild(content);
+      // Add divs inside content div
+      // close button
+      const closeBtn = document.createElement("div");
+      closeBtn.classList.add("close-btn");
+      content.appendChild(closeBtn);
+      const x = document.createElement("p");
+      x.classList.add("x");
+      x.classList.add(`c-x-${movie.idmovie}`);
+      x.innerText = "X";
+      closeBtn.appendChild(x);
+      // item photo
+      const image = document.createElement("img");
+      image.src = movie.strmovieThumb;
+      image.height = "300";
+      image.classList.add("photo");
+      content.appendChild(image);
+      // item name
+      const Iname = document.createElement("p");
+      Iname.classList.add("item-name");
+      Iname.innerHTML = movie.strmovie;
+      content.appendChild(Iname);
+      // div for comments
+      const commnts = document.createElement("div");
+      commnts.classList.add("comment-num");
+      content.appendChild(commnts);
+     
+      // creat form
+      const form = document.createElement("form");
+      form.className = "form_x";
+      form.id = `${movie.idmovie}`;
+      content.appendChild(form);
+      // form title
+      const formTitle = document.createElement("h3");
+      formTitle.className = "form_title";
+      formTitle.innerHTML = "Add a comment";
+      form.appendChild(formTitle);
+      // form name
+      const nameInp = document.createElement("input");
+      nameInp.type = "text";
+      nameInp.id = `name-${movie.idmovie}`;
+      nameInp.placeholder = "Your name";
+      nameInp.className = "form_item";
+      nameInp.required = true;
+      form.appendChild(nameInp);
+      // form textarea
+      const commentText = document.createElement("textarea");
+      commentText.name = "insights";
+      commentText.id = `insight-${movie.idmovie}`;
+      commentText.cols = "30";
+      commentText.rows = "10";
+      commentText.className = "form_item";
+      commentText.placeholder = "Your Comment";
+      commentText.required = true;
+      form.appendChild(commentText);
+      // creat submit button
+      const submitCom = document.createElement("button");
+      submitCom.type = "submit";
+      submitCom.className = "form_item1";
+      submitCom.className = "form_v";
+      submitCom.className = `submit_${movie.idmovie}`;
+      submitCom.innerHTML = "Comment";
+      form.appendChild(submitCom);
+    }
+  });
+};
+export default popCard;
